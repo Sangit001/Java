b26ab870d910a4410ce70c632478d2a9311711f1
Sangit001 <sangit1543@gmail.com>
2025-12-21 22:27:59 +0545

    init commit

---
+++ src/Datatypes.java
@@ -0,0 +1,38 @@
+
+//public class Datatypes{
+
+//public static void main(String[] arg){
+//    int a = 10; //
+//    int a = 1.1 // incorrect cause found float required int
+
+
+//    Premitive datatypes (8)
+//    ****  Numeric datatypes  ****--
+//    Integral datatype = byte, short, int, long
+//    Floating point datatype = float, double
+//    ****  Non-numeric datatypes  **** = char, boolean
+
+
+//    byte
+//    byte: = 1 byte (8bits)
+//    max and min value  and range = +127 to -128
+
+
+//    short : 2 byte (16 bits)
+//    int : 4 byte (32 bits)
+
+//    boolean : unknown
+
+//    char ch = 233; this is correct because each number represent their character
+//    char ch = 97; this display 9
+
+//    Char has 2 byte size because java is UNIC0DE based and its range is 0-65535
+
+
+
+
+
+//    System.out.println(ch);
+//}
+//
+//}
