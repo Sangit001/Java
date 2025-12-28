cff7ce60ab7a4074a91107dca97a62d11865065e
Sangit001 <sangit1543@gmail.com>
2025-12-28 07:32:49 +0545

    init commit

---
+++ src/Abstraction.java
@@ -0,0 +1,60 @@
+// abstract class Test{
+//
+//    abstract void start();
+//    abstract void stop();
+//;
+//}
+//
+//class Remote extends Test{
+//
+//void start(){
+//    System.out.println("Car started");
+//}
+//void stop(){
+//    System.out.println("Car stops");
+//}
+//}
+//
+//
+//
+//
+//
+//public class Abstraction{
+//
+//    public static void main(String[] args){
+//
+//        Test t1 = new Remote();
+//
+//        t1.start();
+//
+//
+//
+//    }
+
+
+abstract class Car{
+
+    abstract void CarNames();
+    abstract void CarColor();
+
+}
+class Details extends Car{
+
+    void CarNames(){
+        System.out.println("BMW M4");
+        System.out.println("TESLA S");
+    }
+    void CarColor(){
+        System.out.println("Black");
+        System.out.println("White");
+    }
+
+}
+public class Abstraction {
+    public static void main(String[] args) {
+
+        Car car1 = new Details();
+        car1.CarNames();
+        car1.CarColor();
+    }
+}
--- src/TypesOfVariables.java
+++ src/TypesOfVariables.java
@@ -121,16 +121,16 @@
 
 //======================= IMPORTANT THING ABOUT INSTANCE AND STATIC VARIABLES ============================
 
-class Test {
-    static int x = 10;
-    int y = 20;
-    public static void main(String[] args){
-
-        Test t1 = new Test();
-        t1.x = 888;
-        t1.y = 999;
-
-        Test t2 = new Test();
-        System.out.println(t2.x+"--"+t2.y);
-    }
-}
+//class Test {
+//    static int x = 10;
+//    int y = 20;
+//    public static void main(String[] args){
+//
+//        Test t1 = new Test();
+//        t1.x = 888;
+//        t1.y = 999;
+//
+//        Test t2 = new Test();
+//        System.out.println(t2.x+"--"+t2.y);
+//    }
+//}
