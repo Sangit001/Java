5c0de6cb98eda588afcebc09d88300d5510d2e22
Sangit001 <sangit1543@gmail.com>
2026-01-03 15:28:31 +0545

    init commit

--- src/OOP/Test.java
+++ src/OOP/Test.java
@@ -1,8 +1,9 @@
 package OOP;
+import java.util.*;
 
 
+// =============================== Data hiding ===============================
 
-// Data hiding
 //public class Test {
 //    private int Serial; // Data hidden
 //
@@ -55,5 +56,106 @@ package OOP;
 //}
 
 
+
+//=============================== Abstraction ===============================
+
+//import java.util.*;
+//
+//
+//    abstract class Account{
+//        abstract void Username();
+//        abstract void Password();
+//        Scanner sc = new Scanner(System.in);
+//    }
+//    class Detail extends Account{
+//
+//        void Username(){
+//            String username = sc.next();
+//        }
+//        void Password(){
+//            String password = sc.next();
+//        }
+//    }
+//public class Test{
+//    public static void main(String[] args){
+//
+//        Account ac = new Detail();
+//        ac.Username();
+//
+//    }
+//}
+
+
+//=============================== Encapsulation ===============================
+
+// class Student {
+//
+//     private String StudentName;
+//     private int StudentRoll;
+//     private int StudentClass;
+//     private double StudentMarks;
+//
+//
+//
+//     // Setter Method
+//     public void setRoll(int RollNumber) {
+//         StudentRoll = RollNumber;
+//     }
+//
+//     public void setName(String studentName) {
+//         StudentName = studentName;
+//     }
+//
+//     public void setClass(int studentClass){
+//
+//     StudentClass = studentClass;
+//     }
+//     public void setMarks(int studentMarks){
+//         StudentMarks = studentMarks;
+//     }
+//
+//     // Getter Method
+//
+//     public int getRoll(){
+//       return  StudentRoll;
+//     }
+//     public String getStudentName(){
+//         return StudentName;
+//     }
+//     public int getStudentClass(){
+//         return StudentClass;
+//     }
+//     public double getStudentMarks(){
+//         return StudentMarks;
+//     }
+// }
+//
+// public class Test{
+//     public static void main (String[] args){
+//         Scanner sc = new Scanner(System.in);
+//
+//         // Taking input data
+//         System.out.println("Please enter Student roll number");
+//         int sRoll = sc.nextInt();
+//         System.out.println("Please enter Student Name");
+//         String sName = sc.next();
+//         System.out.println("Please enter Student Class");
+//         int sClass = sc.nextInt();
+//         System.out.println("Please enter Student Marks");
+//         int sMarks = sc.nextInt();
+//
+//         Student s1 = new Student();
+//         s1.setRoll(sRoll);
+//         s1.setName(sName);
+//         s1.setClass(sClass);
+//         s1.setMarks(sMarks);
+//
+//         System.out.println("Student Roll number is: "+s1.getRoll());
+//         System.out.println("Student Name is: "+s1.getStudentName());
+//         System.out.println("Student Class is: "+s1.getStudentClass());
+//         System.out.println("Student Marks is: "+s1.getStudentMarks());
+//
+//     }
+// }
 
 
