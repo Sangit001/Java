f701cef23e37a29ecf9f04023c46c7b2985de6e2
Sangit001 <sangit1543@gmail.com>
2026-01-03 14:36:33 +0545

    init commit

---
+++ src/OOP/Test.java
@@ -0,0 +1,59 @@
+package OOP;
+
+
+
+// Data hiding
+//public class Test {
+//    private int Serial; // Data hidden
+//
+//    public void setSerial(int s){  // setting and initializing data into s
+//        Serial = s;
+//    }
+//    public int getSerial(){  // Method for returning value
+//        return Serial;
+//    }
+//
+//    public static void main(String[] args){
+//
+//        Test t = new Test();  // creating object of test class
+//        t.setSerial(20);
+//        System.out.println(t.getSerial());
+//
+//    }
+//
+//}
+
+// Creating a program which takes input username and password using Data hiding
+//
+//public class Test{
+//    private String Username;
+//    private String Password;
+//
+//    public void setUsername(String username){
+//        Username = username;
+//
+//
+//    }    public void setPassword(String password){
+//
+//        Password = password;
+//    }
+//    public String getUsername(){
+//        return Username;
+//    }
+//    public String getPassword(){
+//        return Password;
+//    }
+//
+//
+//    public static void main(String[] args){
+//        Test t1 = new Test();
+//        t1.setUsername("Sangit Pandey");
+//        t1.setPassword("Pandey Sangit");
+//        System.out.println("Your password is: "+t1.getPassword());
+//        System.out.println("Your username is: "+t1.getUsername());
+//    }
+//}
+
+
+
+
