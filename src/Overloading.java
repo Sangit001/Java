502ff1ee18d991d073d74cac89ac103c9ce95196
Sangit001 <sangit1543@gmail.com>
2026-01-10 19:40:58 +0545

    init commit

--- src/Overloading.java
+++ src/Overloading.java
@@ -1,25 +1,45 @@
- class animal{
+// class animal{
+//
+// }
+// class monkey{
+//
+// }
+// class Test{
+//    public void m1(animal a){
+//        System.out.println("Animal");
+//    }
+//    public void m1(monkey m){
+//        System.out.println("Monkey");
+//    }
+// }
+//
+//public class Overloading{
+//    public static void main(String[] args){
+//
+//        Test t = new Test();
+//        animal a = new animal();
+//        monkey m = new monkey();
+//        t.m1(a);
+//        t.m1(m);
+//    }
+//}
 
- }
- class monkey{
+//============ General vs Var-arg method
 
- }
- class Test{
-    public void m1(animal a){
-        System.out.println("Animal");
+class Test{
+    public void m1(int x){
+        System.out.println("This is general method");
     }
-    public void m1(monkey m){
-        System.out.println("Monkey");
+    public void m1(int...y){
+        System.out.println("This is var-arg method");
     }
- }
 
-public class Overloading{
-    public static void main(String[] args){
-
-        Test t = new Test();
-        animal a = new animal();
-        monkey m = new monkey();
-        t.m1(a);
-        t.m1(m);
-    }
+}
+public class Overloading {
+public static void main(String[] args){
+    Test t1 = new Test();
+    t1.m1();
+    t1.m1(10);
+    t1.m1(20,30,40);
+}
 }
