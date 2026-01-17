a4bacb86e144e3fe3c40ab0d579133aba2193c3e
Sangit001 <sangit1543@gmail.com>
2026-01-17 19:32:53 +0545

    Overriding with respect to var arg method

---
+++ src/OverridingVarArg.java
@@ -0,0 +1,28 @@
+class P
+{
+    public void m1(int...x){
+        System.out.println("this is parent");
+
+    }
+}
+class C extends P{
+    public void m1(int x){
+        System.out.println("this is child");
+    }
+}
+
+
+
+public class OverridingVarArg {
+    static void main(String[] args) {
+        P p = new P();
+        p.m1(10);
+        C c = new C();
+        c.m1(10);
+        P p1 = new C();
+        p1.m1(10);
+
+    }
+}
+// -------------> in this case this is not overriding it is overloading because to meet the overriding concept,
+// return type and argument must be same there is same return type but not argument
