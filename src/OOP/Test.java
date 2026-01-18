ad78a3426373cca2ce72dbd9575fbee934117018
Sangit001 <sangit1543@gmail.com>
2026-01-18 20:40:14 +0545

    Overloading

--- src/OOP/Test.java
+++ src/OOP/Test.java
@@ -288,3 +288,48 @@ class Test{
 }
  */
 // Though the return type are different but in method signature it does not play any role.
+
+
+
+
+// ================================> OVERLOADING <================================
+/*
+
+    -> Overloading is the process where two methods have same name but different argument type.
+    -> In C programming there is no such concept called Overloading we cannot declare same method name even
+    the argument type are different.
+
+    -> But in Java we can declare multiple method with the same name but, different argument type such type
+    of methods are called Overloaded.
+    Eg:-
+
+    abs (int i)
+    abs (float f)
+    abs (double d)
+
+    -> Overloading concept in Java reduce complexity of a program.
+
+    class Test {
+    public void m1() {
+        System.out.println("non-arg type");
+    }
+
+    public void m1(int i) {
+        System.out.println("int-arg type");
+    }
+
+    public void m1(float f) {
+        System.out.println("floating-point-arg type");
+    }
+
+
+    public static void main(String[] args) {
+        Test t = new Test();
+        t.m1();            //  -> non-arg type
+        t.m1(10);       //  -> int-arg type
+        t.m1(10.5f);   //   -> floating-point-arg type
+    }
+}
+
+
+ */
