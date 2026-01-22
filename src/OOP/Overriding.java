3eefa3151134ebf55fd3531644370671d0082d35
Sangit001 <sangit1543@gmail.com>
2026-01-22 20:58:02 +0545

    Overriding

---
+++ src/OOP/Overriding.java
@@ -0,0 +1,24 @@
+package OOP;
+
+
+class P{
+    public void m1(int x){
+        System.out.println("parent");
+    }
+}
+class C{
+    public void m1(int x){
+        System.out.println("child");
+    }
+}
+public class Overriding {
+    public static void main(String[] args){
+        P p1 = new P();
+        p1.m1(10);
+        C c1 = new C();
+        c1.m1(20);
+        P p2 = new C();
+        p2.m1(12);
+
+    }
+}
