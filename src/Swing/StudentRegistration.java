9af641f28fb98dae3444c923ce12f41d7312d482
Sangit001 <sangit1543@gmail.com>
2026-01-26 00:11:56 +0545

    Student reg completed

--- src/Swing/StudentRegistration.java
+++ src/Swing/StudentRegistration.java
@@ -16,9 +16,9 @@ public class StudentRegistration {
 
         // Name
         JLabel name = new JLabel("Name:");
-        name.setBounds(20,60,50,20);
+        name.setBounds(20,70,50,20);
         JTextField nameValue = new JTextField();
-        nameValue.setBounds(80,60,150,20);
+        nameValue.setBounds(80,70,150,20);
 
         // Age
         Integer[] ageOption = new Integer[50];
@@ -26,15 +26,15 @@ public class StudentRegistration {
                 ageOption[i] = i + 1;
             }
         JLabel age = new JLabel("Age:");
-        age.setBounds(20,85,50,20);
+        age.setBounds(20,95,50,20);
         JComboBox<Integer> ageValue = new JComboBox<>(ageOption);
-        ageValue.setBounds(80,85,150,20);
+        ageValue.setBounds(80,95,150,20);
         frame.add(ageValue);
 
         // For gender
 
         JLabel gender = new JLabel("Gender:");
-        gender.setBounds(20,110,50,20);
+        gender.setBounds(20,120,50,20);
 
 
         frame.add(gender);
@@ -42,37 +42,69 @@ public class StudentRegistration {
 
         // Option for gender
         JRadioButton maleValue = new JRadioButton();
-        maleValue.setBounds(80,110,25,20);
+        maleValue.setBounds(80,120,25,20);
         frame.add(maleValue);
         JLabel male = new JLabel("Male");
-        male.setBounds(105,110,50,20);
+        male.setBounds(105,120,50,20);
         frame.add(male);
         JRadioButton femaleValue = new JRadioButton();
-        femaleValue.setBounds(145,110,25,20);
+        femaleValue.setBounds(145,120,25,20);
         frame.add(femaleValue);
         JLabel female = new JLabel("Female");
-        female.setBounds(170,110,50,20);
+        female.setBounds(170,120,50,20);
         frame.add(female);
 
 
         // Label for hobbies
 
         JLabel hobbies = new JLabel("Hobbies:");
-        hobbies.setBounds(20,135,100,20);
+        hobbies.setBounds(20,145,100,20);
         frame.add(hobbies);
 
         // Checkbox for hobbies
         JCheckBox hobbies1 = new JCheckBox("Playing");
-        hobbies1.setBounds(80, 135, 100,20);
+        hobbies1.setBounds(80, 145, 100,20);
         frame.add(hobbies1);
         JCheckBox hobbies2 = new JCheckBox("Singing");
-        hobbies2.setBounds(180, 135, 100,20);
+        hobbies2.setBounds(180, 145, 100,20);
         frame.add(hobbies2);
         JCheckBox hobbies3 = new JCheckBox("Swim");
-        hobbies3.setBounds(280, 135, 100,20);
+        hobbies3.setBounds(280, 145, 100,20);
         frame.add(hobbies3);
 
-        // Country option
+        // Country label
+        JLabel country = new JLabel("Country:");
+        country.setBounds(20,170,100,20);
+        frame.add(country);
+
+        // Option for Country
+        String[] countryOption = {"Nepal","China","India","Pakistan","United States","United Kingdom","Brazil","Mexico",
+        "Bangladesh"};
+        JComboBox<String> countryValue = new JComboBox<>(countryOption);
+        countryValue.setBounds(80,170,150,20);
+        frame.add(countryValue);
+
+        // Comments
+        JLabel comments = new JLabel("Comment:");
+        comments.setBounds(20,195,100,20);
+        frame.add(comments);
+
+        JTextArea commentBox = new JTextArea();
+        commentBox.setBounds(90,195,150,50);
+        frame.add(commentBox);
+
+        // Creating buttons
+
+        //Submit btn
+        JButton submitBtn = new JButton("Submit");
+        submitBtn.setBounds(80,270,75,30);
+        frame.add(submitBtn);
+
+        // Cancel btn
+        JButton cancelBtn = new JButton("Cancel");
+        cancelBtn.setBounds(150,270,75,30);
+        frame.add(cancelBtn);
+
 
 
 
