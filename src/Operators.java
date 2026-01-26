c5e6b686fc9c8aff95960a7b65bd90dd2f436ee1
Sangit001 <sangit1543@gmail.com>
2026-01-26 22:25:39 +0545

    Operators

---
+++ src/Operators.java
@@ -0,0 +1,33 @@
+public class Operators {
+
+    public static void main(String[] args){
+
+    /*
+    ======================== Increment and Decrement Operator
+
+    int x = 10;
+    int y = ++x;
+    System.out.println(y);      // 11
+
+    int x = 10;
+    int y = ++10;               // needs variable not constant value
+    System.out.println(y);
+
+    int x = 10;
+    int y = ++(++x);            // Error CE
+    System.out.println(y);
+
+    First (++x) will become 11 and then ++ operator can't increment const value
+
+        --------- final ----------
+        final x = 10;
+        x++;                    Can't assign in final keyword because it is the final value and final is constant.
+
+                                but we can assign increment/decrement operator except boolean
+     */
+
+//        char c = 'a';
+        System.out.println("Hello java");
+
+    }
+}
