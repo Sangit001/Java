87f1b786503282638a9e0fd595b6dd77ab6f261a
Sangit001 <sangit1543@gmail.com>
2026-01-26 23:23:24 +0545

    arithmetic operator

---
+++ src/OperatorsAndAssignment/ArithmeticOperator.java
@@ -0,0 +1,33 @@
+package OperatorsAndAssignment;
+
+public class ArithmeticOperator {
+    public static void main(String[] args) {
+//        (+,-,*,/,%)
+
+/*
+        If we apply any arithmetic operator between two variables (a and b) the result type will always.
+        MAX(int, type of a, type of b)
+
+        byte + byte = int
+        byte + short = int
+        short + short = int
+        byte + long = long
+        long + double = double
+        float + long = float
+        char + char = int
+        char + double = double
+
+        Examples:-
+                    System.out.println('a' + 'b');  ----> 97+98 = 195
+                    System.out.println('a' + 0.89); ----> 97+0.89 = 97.89
+
+        Chart:-
+                    byte --> short --> int --> long --> float --> double
+                    char --> int --> long --> float --> double
+
+
+
+
+ */
+    }
+}
