1cd428ae83474c129f419b41c87c18812e0d1d3b
Sangit001 <sangit1543@gmail.com>
2026-01-27 21:46:56 +0545

    Overloaded operator

---
+++ src/OperatorsAndAssignment/OverloadedOperator.java
@@ -0,0 +1,24 @@
+package OperatorsAndAssignment;
+
+public class OverloadedOperator {
+    public static void main (String[] args){
+/*
+
+        The only overloaded operator in Java is (+) operator. Sometime it access arithmetic addition operator
+        and sometime it access String concatination operator.
+
+        If atleast one argument is String type (+) operator access concatination operator and if both arguments are
+        number type then (+) operator access arithmetic operator.
+
+        Example:-   String  a = "Sangit";
+                    int     b = 10, c = 20, d = 30;
+                    System.out.println(a+b+c+d);      Sangit102030
+                    System.out.println(b+c+d+a);    60Sangit
+                    System.out.println(b+c+a+d);    30Sangit30
+                    System.out.print(b+a+c+d);      10Sangit2030
+
+
+ */
+
+    }
+}
