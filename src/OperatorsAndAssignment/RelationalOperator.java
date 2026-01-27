986a8894b8918f7d038e96eee8e7054ea9b8c584
Sangit001 <sangit1543@gmail.com>
2026-01-27 22:16:34 +0545

    Overloaded operator

---
+++ src/OperatorsAndAssignment/RelationalOperator.java
@@ -0,0 +1,26 @@
+package OperatorsAndAssignment;
+
+public class RelationalOperator {
+    public static void main(String[] args){
+/*
+
+        --->    Relational operator can't be used in boolean.
+
+                Example:-   System.out.println(trut>false);
+        --->    We can't use relational operators for object types
+                Example:-   System.out.println("sangit123">"durga");
+
+        --->    Nesteing of Relatinal operator is not allowed otherwise we will get compile time error.
+                Example:-   System.out.println(10<20<30);
+                            (10<20) is true but (true<30) is false
+                            Compile time error:-    Operator < cannot be applied to boolean, int
+
+
+
+
+
+
+ */
+
+    }
+}
